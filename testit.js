e0fad05780937dce63117e0692e979d284311a90
Andreas Lind <andreas@one.com>
2017-02-01 23:23:29 +0100

    Add 'to throw' example.

--- testit.js
+++ testit.js
@@ -15,4 +15,10 @@ describe('the test', function () {
     it('should work with multi-argument assertions', function () {
         expect(88, 'to be within', 1, 50);
     });
+
+    it('should work with "to throw"', function () {
+        expect(function () {
+            throw new Error('oh dear');
+        }, 'to throw', 'oh no');
+    });
 });
