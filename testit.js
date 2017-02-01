2a3413368f36b4b89d1bbf8e93e3e5d189b5648e
Andreas Lind <andreas@one.com>
2017-02-01 21:49:27 +0100

    testit: Add 'to satisfy' case.

--- testit.js
+++ testit.js
@@ -4,4 +4,11 @@ describe('the test', function () {
     it('should foo', function () {
         expect('foo', 'to equal', 'bar');
     });
+
+    it('should work with a complex to satisfy', function () {
+        expect({foo: 123, bar: 456}, 'to satisfy', {
+            foo: 123,
+            bar: 789
+        });
+    });
 });
