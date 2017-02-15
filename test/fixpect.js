5acd4404a09bece4ba02e633ada4754f9316e3a1
Andreas Lind <andreas@one.com>
2017-02-15 08:36:55 +0100

    Unskip test that now passes with the update Unexpected.

--- test/fixpect.js
+++ test/fixpect.js
@@ -171,7 +171,7 @@ describe('fixpect', function () {
             `);
         });
 
-        it.skip('should fix a complex compound assertion', function () {
+        it('should fix a complex compound assertion', function () {
             return expect(`
                 function delayedIncrement(num, cb) {
                     setTimeout(function () {
