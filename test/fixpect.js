abedee16fc9f68c3d6646d69fe435078a9f72654
Andreas Lind <andreaslindpetersen@gmail.com>
2018-09-26 23:36:32 +0200

    eslint --fix .

--- test/fixpect.js
+++ test/fixpect.js
@@ -3,45 +3,78 @@ const pathModule = require('path');
 const fs = expect.promise.promisifyAll(require('fs'));
 const _ = require('lodash');
 const childProcess = require('child_process');
-const preamble = "var expect = require('unexpected').clone().use(require('" + pathModule.resolve(__dirname, '..', 'lib', 'fixpect.js') + "'));\n";
+const preamble = `var expect = require('unexpected').clone().use(require('${pathModule.resolve(
+  __dirname,
+  '..',
+  'lib',
+  'fixpect.js'
+)}'));\n`;
 
 const tmpDir = pathModule.resolve(__dirname, 'tmp');
 
 before(() => fs.mkdirAsync(tmpDir).catch(() => {}));
 after(() => fs.rmdirAsync(tmpDir).catch(() => {}));
 
-expect.addAssertion('<string> to come out as <string>', (expect, subject, value) => {
-    const tmpFileName = pathModule.resolve(tmpDir, 'fixpect' + Math.round(10000000 * Math.random()) + '.js');
-    const testCommand = process.argv[0] + ' ' + pathModule.resolve(__dirname, '..', 'node_modules', '.bin', 'mocha') + ' ' + tmpFileName;
+expect.addAssertion(
+  '<string> to come out as <string>',
+  (expect, subject, value) => {
+    const tmpFileName = pathModule.resolve(
+      tmpDir,
+      `fixpect${Math.round(10000000 * Math.random())}.js`
+    );
+    const testCommand = `${process.argv[0]} ${pathModule.resolve(
+      __dirname,
+      '..',
+      'node_modules',
+      '.bin',
+      'mocha'
+    )} ${tmpFileName}`;
 
-    return fs.writeFileAsync(tmpFileName, preamble + subject, 'utf-8')
-        .then(() => expect.promise.fromNode(cb => childProcess.exec(testCommand, {env: _.defaults({FIXPECT: 'yes'}, process.env)}, cb.bind(null, null))))
-        .then(([err, stdout, stderr]) => {
-            if (err && err.code === 165) {
-                throw new Error('fixpect failed with: ' + stdout);
-            }
-        })
-        .then(() => fs.readFileAsync(tmpFileName, 'utf-8'))
-        .then(contents => expect(contents.substr(preamble.length), 'to equal', value))
-        .finally(() => fs.unlinkAsync(tmpFileName));
-});
+    return fs
+      .writeFileAsync(tmpFileName, preamble + subject, 'utf-8')
+      .then(() =>
+        expect.promise.fromNode(cb =>
+          childProcess.exec(
+            testCommand,
+            { env: _.defaults({ FIXPECT: 'yes' }, process.env) },
+            cb.bind(null, null)
+          )
+        )
+      )
+      .then(([err, stdout, stderr]) => {
+        if (err && err.code === 165) {
+          throw new Error(`fixpect failed with: ${stdout}`);
+        }
+      })
+      .then(() => fs.readFileAsync(tmpFileName, 'utf-8'))
+      .then(contents =>
+        expect(contents.substr(preamble.length), 'to equal', value)
+      )
+      .finally(() => fs.unlinkAsync(tmpFileName));
+  }
+);
 
-describe('fixpect', function () {
-    describe('to equal', function () {
-        it('should fix a failing string comparison', function () {
-            return expect(`
+describe('fixpect', function() {
+  describe('to equal', function() {
+    it('should fix a failing string comparison', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     expect('foo', 'to equal', 'bar');
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     expect('foo', 'to equal', 'foo');
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix a failing multiline string comparison', function () {
-            return expect(`
+    it('should fix a failing multiline string comparison', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     var str = '';
                     for (var i = 0 ; i < 20 ; i += 1) {
@@ -53,7 +86,9 @@ describe('fixpect', function () {
                         ''
                     );
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     var str = '';
                     for (var i = 0 ; i < 20 ; i += 1) {
@@ -84,59 +119,77 @@ describe('fixpect', function () {
                         'abcdefghijklmnopqrstuvwxyz1234567890\\n'
                     );
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix a failing number comparison', function () {
-            return expect(`
+    it('should fix a failing number comparison', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     expect(123, 'to equal', 456);
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     expect(123, 'to equal', 123);
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix a failing object comparison', function () {
-            return expect(`
+    it('should fix a failing object comparison', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     expect({a: 456, b: {c: 789}}, 'to equal', 456);
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     expect({a: 456, b: {c: 789}}, 'to equal', { a: 456, b: { c: 789 } });
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should leave a failing "not to equal" alone', function () {
-            return expect(`
+    it('should leave a failing "not to equal" alone', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     expect('foo', 'not to equal', 'foo');
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     expect('foo', 'not to equal', 'foo');
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix a failing "to satisfy" comparison', function () {
-            return expect(`
+    it('should fix a failing "to satisfy" comparison', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     expect({ a: 456, b: { c: 789 } }, 'to satisfy', { b: 123 });
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     expect({ a: 456, b: { c: 789 } }, 'to satisfy', { b: { c: 789 } });
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix a failing "to exhaustively satisfy" comparison', function () {
-            return expect(`
+    it('should fix a failing "to exhaustively satisfy" comparison', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     expect({
                         foo: 'foo',
@@ -147,7 +200,9 @@ describe('fixpect', function () {
                         bar: 'bar'
                     });
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     expect({
                         foo: 'foo',
@@ -155,11 +210,13 @@ describe('fixpect', function () {
                         quux: 'quux'
                     }, 'to exhaustively satisfy', { foo: 'foo', bar: 'bar', quux: 'quux' });
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix an "<array> to satisfy <object>" assertion', function () {
-            return expect(`
+    it('should fix an "<array> to satisfy <object>" assertion', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     expect([
                         'foo',
@@ -167,7 +224,9 @@ describe('fixpect', function () {
                         'quux'
                     ], 'to satisfy', { 1: 'baz' });
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     expect([
                         'foo',
@@ -175,11 +234,13 @@ describe('fixpect', function () {
                         'quux'
                     ], 'to satisfy', { 1: 'bar' });
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix an "<array> to satisfy <array>" assertion', function () {
-            return expect(`
+    it('should fix an "<array> to satisfy <array>" assertion', function() {
+      return expect(
+        `
                 it('should foo', function () {
                     expect([
                         'foo',
@@ -187,7 +248,9 @@ describe('fixpect', function () {
                         'quux'
                     ], 'to satisfy', [ 'foo', 'bar' ]);
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 it('should foo', function () {
                     expect([
                         'foo',
@@ -195,11 +258,13 @@ describe('fixpect', function () {
                         'quux'
                     ], 'to satisfy', [ 'foo', 'bar', 'quux' ]);
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix up an assertion with a "middle of the rocket" assertion', function () {
-            return expect(`
+    it('should fix up an assertion with a "middle of the rocket" assertion', function() {
+      return expect(
+        `
                 expect.addAssertion('<any> when delayed a little bit <assertion>', function (expect, subject) {
                     return expect.promise(function (run) {
                         setTimeout(run(function () {
@@ -210,7 +275,9 @@ describe('fixpect', function () {
                 it('should foo', function () {
                     return expect(123, 'when delayed a little bit', 'to equal', 456);
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 expect.addAssertion('<any> when delayed a little bit <assertion>', function (expect, subject) {
                     return expect.promise(function (run) {
                         setTimeout(run(function () {
@@ -221,11 +288,13 @@ describe('fixpect', function () {
                 it('should foo', function () {
                     return expect(123, 'when delayed a little bit', 'to equal', 123);
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix up a compound assertion', function () {
-            return expect(`
+    it('should fix up a compound assertion', function() {
+      return expect(
+        `
                 expect.addAssertion('<any> when delayed a little bit <assertion>', function (expect, subject) {
                     return expect.promise(function (run) {
                         setTimeout(run(function () {
@@ -236,7 +305,9 @@ describe('fixpect', function () {
                 it('should foo', function () {
                     return expect(123, 'when delayed a little bit to equal', 456);
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 expect.addAssertion('<any> when delayed a little bit <assertion>', function (expect, subject) {
                     return expect.promise(function (run) {
                         setTimeout(run(function () {
@@ -247,11 +318,13 @@ describe('fixpect', function () {
                 it('should foo', function () {
                     return expect(123, 'when delayed a little bit to equal', 123);
                 });
-            `);
-        });
+            `
+      );
+    });
 
-        it('should fix a complex compound assertion', function () {
-            return expect(`
+    it('should fix a complex compound assertion', function() {
+      return expect(
+        `
                 function delayedIncrement(num, cb) {
                     setTimeout(function () {
                         if (typeof num === 'number') {
@@ -264,7 +337,9 @@ describe('fixpect', function () {
                 it('should foo', function () {
                     return expect([123], 'when passed as parameters to async', delayedIncrement, 'to equal', 125);
                 });
-            `, 'to come out as', `
+            `,
+        'to come out as',
+        `
                 function delayedIncrement(num, cb) {
                     setTimeout(function () {
                         if (typeof num === 'number') {
@@ -277,47 +352,56 @@ describe('fixpect', function () {
                 it('should foo', function () {
                     return expect([123], 'when passed as parameters to async', delayedIncrement, 'to equal', 124);
                 });
-            `);
-        });
-
-        it('should not fix a failing expect inside another expect', function () {
-            return expect(`
-                it('should foo', function () {
-                    expect(function () {
-                        expect(123, 'to equal', 456);
-                    }, 'to throw', 'expected 123 to equal 456');
-                });
-            `, 'to come out as', `
-                it('should foo', function () {
-                    expect(function () {
-                        expect(123, 'to equal', 456);
-                    }, 'to throw', 'expected 123 to equal 456');
-                });
-            `);
-        });
-
-        it('should not fix a failing expect inside another async expect', function () {
-            return expect(`
-                it('should foo', function () {
-                    return expect(function () {
-                        return expect.promise(function (run) {
-                            setImmediate(run(function () {
-                                expect(123, 'to equal', 456);
-                            }));
-                        });
-                    }, 'to error');
-                });
-            `, 'to come out as', `
-                it('should foo', function () {
-                    return expect(function () {
-                        return expect.promise(function (run) {
-                            setImmediate(run(function () {
-                                expect(123, 'to equal', 456);
-                            }));
-                        });
-                    }, 'to error');
-                });
-            `);
-        });
+            `
+      );
     });
+
+    it('should not fix a failing expect inside another expect', function() {
+      return expect(
+        `
+                it('should foo', function () {
+                    expect(function () {
+                        expect(123, 'to equal', 456);
+                    }, 'to throw', 'expected 123 to equal 456');
+                });
+            `,
+        'to come out as',
+        `
+                it('should foo', function () {
+                    expect(function () {
+                        expect(123, 'to equal', 456);
+                    }, 'to throw', 'expected 123 to equal 456');
+                });
+            `
+      );
+    });
+
+    it('should not fix a failing expect inside another async expect', function() {
+      return expect(
+        `
+                it('should foo', function () {
+                    return expect(function () {
+                        return expect.promise(function (run) {
+                            setImmediate(run(function () {
+                                expect(123, 'to equal', 456);
+                            }));
+                        });
+                    }, 'to error');
+                });
+            `,
+        'to come out as',
+        `
+                it('should foo', function () {
+                    return expect(function () {
+                        return expect.promise(function (run) {
+                            setImmediate(run(function () {
+                                expect(123, 'to equal', 456);
+                            }));
+                        });
+                    }, 'to error');
+                });
+            `
+      );
+    });
+  });
 });
